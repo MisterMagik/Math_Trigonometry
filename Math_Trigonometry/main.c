7e478a0dee0c1c73be241a1b31ee8165f3a9cc94
PC <PC@DESKTOP-U2IEPOS>
2022-02-20 17:00:59 +0100

    Add project files.

---
+++ Math_Trigonometry/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define SIN	0.0000
+#define COS	0.9998
+#define TG	0.0175
+#define CTG	57.290
+
+
+int main()
+{
+	printf("##################\n");
+	printf("# Trigonometry   #\n");
+	printf("##################\n");
+
+
+	return 0;
+}
